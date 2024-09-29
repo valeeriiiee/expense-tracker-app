2deed043c3d9022d9c557a6a04adee85311ce96f
valeeriiiee <obleavalerie@gmail.com>
2024-09-29 16:30:05 +0800

    Create PieChart.jsx

---
+++ src/components/PieChart/PieChart.jsx
@@ -0,0 +1,35 @@
+// components/PieChart/PieChart.jsx
+import { PieChart, Pie, Cell } from 'recharts';
+import { useMemo } from 'react';
+
+export const Chart = ({ data }) => {
+  const chartData = useMemo(() => data, [data]);
+
+  return (
+    <PieChart width={292} height={150}>
+      <Pie
+        dataKey="value"
+        data={chartData}
+        startAngle={180}
+        endAngle={0}
+        cy={140}
+        innerRadius={95}
+        outerRadius={145}
+        paddingAngle={-10}
+        cornerRadius={10}
+        fill="#15151b"
+        stroke="none"
+      >
+        {chartData.map((entry, index) => (
+          <Cell
+            style={{ outline: 'none' }}
+            key={`cell-${index}`}
+            fill={entry.color}
+          />
+        ))}
+      </Pie>
+    </PieChart>
+  );
+};
+
+export default Chart;
