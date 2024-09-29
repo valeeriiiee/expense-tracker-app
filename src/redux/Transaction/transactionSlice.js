f8ae991238356f7cbdfd839664fc209b54edc194
valeeriiiee <obleavalerie@gmail.com>
2024-09-29 16:34:36 +0800

    Create transactionSlice.js

---
+++ src/redux/Transaction/transactionSlice.js
@@ -0,0 +1,108 @@
+import { createSlice, isAnyOf } from '@reduxjs/toolkit';
+
+import {
+  getTransactions,
+  addTransaction,
+  editTransaction,
+  deleteTransaction,
+} from './operations';
+import { logOut } from '../Auth/operations';
+
+const initialState = {
+  transactions: null,
+  totalTransExpenses: null,
+  totalTransIncomes: null,
+  error: null,
+};
+
+const transactionSlice = createSlice({
+  name: 'transactions',
+  initialState,
+  extraReducers: builder =>
+    builder
+      .addCase(getTransactions.fulfilled, (state, { payload }) => {
+        const { data, date, type } = payload;
+        state.transactions = data;
+        if (!data.length && type === 'incomes') {
+          state.totalTransIncomes = 0;
+        }
+        if (!data.length && type === 'expenses') {
+          state.totalTransExpenses = 0;
+        }
+
+        if (data.length && !date) {
+          if (type === 'expenses') {
+            state.totalTransExpenses = data.reduce((total, item) => {
+              return (total += item.sum);
+            }, 0);
+          } else {
+            state.totalTransIncomes = data.reduce((total, item) => {
+              return (total += item.sum);
+            }, 0);
+          }
+        }
+        state.error = null;
+      })
+      .addCase(addTransaction.fulfilled, (state, action) => {
+        state.error = null;
+        if (state.transactions === null) {
+          state.transactions = [action.payload];
+        } else state.transactions = [...state.transactions, action.payload];
+      })
+
+      .addCase(editTransaction.fulfilled, (state, action) => {
+        state.transactions = state.transactions.map(item => {
+          if (item._id === action.payload._id) {
+            return action.payload;
+          }
+          return item;
+        });
+        state.error = null;
+      })
+      .addCase(deleteTransaction.fulfilled, (state, action) => {
+        state.transactions = state.transactions.filter(
+          item => item._id !== action.payload
+        );
+        state.error = null;
+      })
+      .addCase(logOut.fulfilled, state => {
+        return initialState;
+      })
+      .addMatcher(
+        isAnyOf(
+          getTransactions.pending,
+          addTransaction.pending,
+          editTransaction.pending,
+          deleteTransaction.pending
+        ),
+        state => {
+          state.error = null;
+        }
+      )
+      .addMatcher(
+        isAnyOf(
+          getTransactions.rejected,
+          addTransaction.rejected,
+          editTransaction.rejected,
+          deleteTransaction.rejected
+        ),
+        (state, action) => {
+          state.error = action.payload;
+        }
+      ),
+  selectors: {
+    selectTransactions: state => state.transactions,
+    selectTransactionsError: state => state.error,
+    selectTotalTransExpenses: state => state.totalTransExpenses,
+    selectTotalTransIncomes: state => state.totalTransIncomes,
+  },
+});
+
+export const {
+  selectTransactions,
+  selectTransactionsError,
+  selectTotalTransExpenses,
+  selectTotalTransIncomes,
+} = transactionSlice.selectors;
+
+export const transactionSliceReducer = transactionSlice.reducer;
